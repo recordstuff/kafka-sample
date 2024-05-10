b4eaa8893066ea6d1618e8af64a1fdfd3e003a2d
Brett Drake <recordstuff@hotmail.com>
2024-05-10 05:09:26 -0500

    update to different version of kafka image

--- KafkaSample/KafkaSample/Services/KafkaConsumerService.cs
+++ KafkaSample/KafkaSample/Services/KafkaConsumerService.cs
@@ -1,4 +1,5 @@
 ﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
 
 namespace KafkaSample.Services;
 
@@ -11,7 +12,6 @@ public class KafkaConsumerService : BackgroundService, IHostedService
     public KafkaConsumerService(ILogger<KafkaConsumerService> logger)
     {
         _logger = logger;
-        
         _config = new ConsumerConfig
         {
             BootstrapServers = "kafka:29092",
@@ -19,12 +19,9 @@ public class KafkaConsumerService : BackgroundService, IHostedService
             AutoOffsetReset = AutoOffsetReset.Earliest
         };
 
-
-
         _consumer = new ConsumerBuilder<Ignore, string>(_config).Build();
-        _consumer.Subscribe("test-topic");
     }
-    
+
     public override Task StopAsync(CancellationToken cancellationToken)
     {
         _consumer.Close();
@@ -35,13 +32,34 @@ public class KafkaConsumerService : BackgroundService, IHostedService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var config = new AdminClientConfig
+        {
+            BootstrapServers = "kafka:29092"
+        };
+
+        using (var adminClient = new AdminClientBuilder(config).Build())
+        {
+            try
+            {
+                await adminClient.CreateTopicsAsync(new List<TopicSpecification>
+                {
+                    new TopicSpecification
+                    {
+                        Name = "test-topic",
+                        NumPartitions = 1,
+                        ReplicationFactor = 1
+                    }
+                });
+            }
+            catch (CreateTopicsException e)
+            {
+                Console.WriteLine($"An error occurred creating the topic: {e.Results[0].Error.Reason}");
+            }
+        }
+
+        _consumer.Subscribe("test-topic");
+
         await Task.Yield();
-
-        // give time for the kafka container to auto create the test-topic
-
-        Thread.Sleep(3000);
-
-        // todo use health checks in docker compose?
 
         _logger.LogInformation($"Blocking on background thread for test-topic.");
 
